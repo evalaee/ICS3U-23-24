a1c1b4d8d9605ea9a3a478fcc66d973d92b50ad5
Elina Valaee <evalaee@bayviewglen.ca>
2023-09-11 12:38:49 -0400

    HomeworkOne/Two

---
+++ Unit One- Introduction to Java/src/lesson2/HomeworkOne.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class HomeworkOne {
+    public static void main(String[] args) {
+        System.out.println("What phobia did Alfred Hitchcock suffer from?");
+        System.out.println("A- Aerophobia (fear of flying)");
+        System.out.println("B- Ovophobia (fear of eggs)");
+        System.out.println("C- Arachibutyrophobia (fear of peanut butter sticking to the roof of your mouth)");
+        System.out.println("D- Arithmophobia (fear of numbers)");
+        System.out.print("Correct Answer: B- Ovophobia");
+    }
+}
---
+++ Unit One- Introduction to Java/src/lesson2/HomeworkTwo.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class HomeworkTwo {
+    public static void main(String[] args) {
+        System.out.println("Exploring Huron-Wendat Heritage");
+        System.out.println("Key aspects of Huron-Wendat culture:");
+        System.out.println("- Traditional Practices,");
+        System.out.println("- Art and Craftsmanship");
+        System.out.println("- Language and Storytelling");
+        System.out.println("- Community Traditions");
+        System.out.println("Date: Monday, September 11, 2023");
+        System.out.println("Duration: 60 minutes");
+    }
+}
