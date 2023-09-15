3c4340fd3ff60523db20773c1f0f83fd604f9674
Elina Valaee <evalaee@bayviewglen.ca>
2023-09-15 11:22:21 -0400

    lesson3

---
+++ lesson3/ExampleFive.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class ExampleFive {
+    public static void main(String[] args) {
+        double mark1 = 80, mark2 = 88, mark3 = 97;
+
+        double average = (mark1 + mark2 + mark3) /3.0;
+        // we need to divide by 3.0 so we do double math
+        System.out.println(average);
+    }
+}
---
+++ lesson3/HwOne.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class HwOne {
+   public static void main(String[] args) {
+    double item1 = 15.95;
+    double item2 = 15.95;
+    double item3 = 15.95;
+
+    double sum = item1 + item2 + item3;
+
+    System.out.println(sum);
+   }
+}
